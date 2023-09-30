8ce716977fbc7370f97b82971c3d86e6c9a9876a
IrmaAdams <strydom.irma@gmail.com>
2023-09-30 12:13:40 +0100

    add Description for Project

--- Java Encryption Program/Java Encryption program/src/Main.java
+++ Java Encryption Program/Java Encryption program/src/Main.java
@@ -1,5 +1,25 @@
+/* ----------------------------------------------------------------------------------------
+Encryption Program
+By Irma Adams
+30/09/2023
+
+*   OTP =   One-time pad
+            encryption technique that cannot be cracked,
+            but requires the use of a one-time pre-shared
+            key the same size as, or longer than, the message
+            being sent.  In this technique, plaintext is paired
+            with a random secret key (also referred to as a one-time pad).
+            -Wikipedia-
+
+*   I will be replacing each letter in the plain text message with a random one from a key that
+    the program will generate.
+*   I will be utilising the ASCII Table (American Standard Code for Information Interchange)
+------------------------------------------------------------------------------------------ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+
     }
 }
