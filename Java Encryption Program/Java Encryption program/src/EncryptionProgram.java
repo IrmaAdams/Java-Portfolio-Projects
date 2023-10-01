56f405094e1a8715357b1878cf7336244b293e00
IrmaAdams <strydom.irma@gmail.com>
2023-10-01 14:54:36 +0100

    Added notes, completed project

--- Java Encryption Program/Java Encryption program/src/EncryptionProgram.java
+++ Java Encryption Program/Java Encryption program/src/EncryptionProgram.java
@@ -56,32 +56,34 @@ public class EncryptionProgram {
         }
     }
     private void newKey(){
-        character = ' ';
-        // clear old lists before generating a new key:
-        list.clear();
-        shuffledList.clear();
+        character = ' ';                // ASCII index for space is 32
+        list.clear();                   // clear old lists before generating a new key
+        shuffledList.clear();           // clear old lists before generating a new key
 
-        for(int i = 32; i<127; i++){ // using characters between 32 and 127 on the ASCII table
-            list.add(Character.valueOf(character));
-            character++;
+        for(int i = 32; i<127; i++){                    // using characters between 32 and 127 on the ASCII table
+            list.add(Character.valueOf(character));     // take the empty list, and then add the character value of our character
+            character++;                                // increment our character by 1
         }
 
-        // create a new array list:
-        shuffledList = new ArrayList<>(list);
-        // to shuffle the new array list:
-        Collections.shuffle(shuffledList);
+        // How to make a copy of an array list:
+        shuffledList = new ArrayList<>(list);           // initiate shuffledList[] by creating a new copy of list[]
+        Collections.shuffle(shuffledList);              // method to shuffle the new array list
+
         System.out.println("*A new key has been generated*\n");
 
     }
     private void getKey(){
-        System.out.println("Key: ");
-        for(Character x : list){
-            System.out.print(x);
+        System.out.println("Key: ");                    // display the new key (list of shuffled characters)
+
+        for(Character x : list){                        // for each character x in our list,
+            System.out.print(x);                        // print out character at index x
         }
-        for(Character y: shuffledList){
-            System.out.print(y);
+
+        for(Character y: shuffledList){                 // for each character y in our shuffledList,
+            System.out.print(y);                        // print out character at index y
         }
-        System.out.println();
+
+        System.out.println();                           // add a space after with a prinln statement
 
     }
     private void encrypt(){
@@ -92,19 +94,51 @@ public class EncryptionProgram {
         letters = message.toCharArray();
 
         // create a for loop to iterate through each character in the array of characters called 'letters'
-        for(int i=0; i<letters.length;i++){             
-            for(int j=0;j<list.size();j++){
-                if(letters[i]==list.get(j)){
-                    letters[i]=shuffledList.get(j);
-                    break;
+        for(int i=0; i<letters.length;i++){                 // create a for loop to iterate through each character in our array of characters called letters
+            for(int j=0;j<list.size();j++){                 // nested for loop to look through the list to see if there
+                                                                // is a matching letter to our current letter that we're looking at, within our array
+                if(letters[i]==list.get(j)){                // find the character in list[] and retrieve corresponding character at index in shuffledList[]
+                    letters[i]=shuffledList.get(j);         // take that letter and replace it with the character at index i in shuffledList[]
+                    break;                                  // then break out of the loop
                 }
             }
         }
+        System.out.println("Encrypted: ");                  // display the encrypted message
+        for(char x : letters){                              // for each loop to iterate through each letter in this array of characters
+                                                            // for each char at index x in letters,
+            System.out.print(x);                            // print character at index x
+        }
+        System.out.println();                               // add a space after with a prinln statement
     }
     private void decrypt(){
+        // Decryption process is a similar process to the Decrypt method
 
+        System.out.println("Enter the message you would like to decrypt: ");
+        String message = scanner.nextLine();
+
+        // break up the message string into an array of characters:
+        letters = message.toCharArray();
+
+        // create a for loop to iterate through each character in the array of characters called 'letters'
+        for(int i=0; i<letters.length;i++){                 // create a for loop to iterate through each character in our array of characters called letters
+            for(int j=0;j<shuffledList.size();j++){         // nested for loop to look through the shuffledList to see if there
+                                                                // is a matching letter to our current letter that we're looking at, within our array
+                if(letters[i]==shuffledList.get(j)){        // find the character in shuffledList[] and retrieve corresponding character at index in list[]
+                    letters[i]=list.get(j);                 // take that letter and replace it with the character at index i in list[]
+                    break;                                  // then break out of the loop
+                }
+            }
+        }
+        System.out.println("Decrypted: ");                  // display the decrypted message
+        for(char x : letters){                              // for each loop to iterate through each letter in this array of characters
+                                                                // for each char at index x in letters,
+            System.out.print(x);                            // print character at index x
+        }
+        System.out.println();                               // add a space after with a prinln statement
     }
     private void quit(){
-
+        // Close the program
+        System.out.println("Thank you.  Have a nice day");
+        System.exit(0);                              // exit out of program
     }
 }
