af76d622f6df02eae1f7041c62bca2b82ef3daa5
IrmaAdams <strydom.irma@gmail.com>
2023-10-07 13:29:16 +0100

    project completed - text editor

--- Java Text Editor app/src/TextEditor.java
+++ Java Text Editor app/src/TextEditor.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TextEditor extends JFrame implements ActionListener {
 
@@ -20,32 +25,31 @@ public class TextEditor extends JFrame implements ActionListener {
     JMenuItem saveItem;
     JMenuItem exitItem;
 
-    TextEditor(){
+    TextEditor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
-        this.setSize(500,500);
+        this.setSize(500, 500);
         this.setLayout(new FlowLayout());
         this.setLocationRelativeTo(null);
 
         textArea = new JTextArea();
-//        textArea.setPreferredSize(new Dimension(450,450));
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         textArea.setFont(new Font("Ariel", Font.PLAIN, 20));
 
         scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(450,450));
+        scrollPane.setPreferredSize(new Dimension(450, 450));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
         fontLabel = new JLabel("Font: ");
 
         fontSizeSpinner = new JSpinner();
-        fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+        fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
         fontSizeSpinner.setValue(20);
         fontSizeSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN,(int)fontSizeSpinner.getValue()));
+                textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
             }
         });
 
@@ -57,6 +61,27 @@ public class TextEditor extends JFrame implements ActionListener {
         fontBox.addActionListener(this);
         fontBox.setSelectedItem("Ariel");
 
+        //------- Menu Bar -------
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+
+        //------- Menu Bar -------
+
+        this.setJMenuBar(menuBar);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
         this.add(fontColourButton);
@@ -64,16 +89,72 @@ public class TextEditor extends JFrame implements ActionListener {
         this.add(scrollPane);
         this.setVisible(true);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==fontColourButton){
+        if (e.getSource() == fontColourButton) {
             JColorChooser colorChooser = new JColorChooser();
 
-            Color colour = colorChooser.showDialog(null, "Choose a colour", Color.BLACK);
+            Color colour = JColorChooser.showDialog(null, "Choose a colour", Color.BLACK);
             textArea.setForeground(colour);
         }
-        if(e.getSource()==fontBox){
-            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN, textArea.getFont().getSize()));
+        if (e.getSource() == fontBox) {
+            textArea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
         }
+
+        if(e.getSource()==openItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+            if(response==JFileChooser.APPROVE_OPTION){
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()){
+                        while(fileIn.hasNextLine()){
+                            String line = fileIn.nextLine()+"\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                finally {
+                    assert fileIn != null;
+                    fileIn.close();
+                }
+            }
+        }
+        if(e.getSource()==saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+
+            if(response==JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                finally {
+                    fileOut.close();
+                }
+            }
+        }
+        if(e.getSource()==exitItem){
+            System.exit(0);
+        }
+
     }
 }
